1e64d398d50633e5337dda09998625f9ebc4cdc1
Aswin V g <97618263+AswinVG-source@users.noreply.github.com>
2026-04-17 10:36:30 +0530

    Fix typos in footer.js

---
+++ footer.js
@@ -0,0 +1,21 @@
+// Updated code in footer.js
+
+// Your previous code...
+function formaTime(time) {
+    // Some code here
+}
+
+// Updated function name
+function formatTime(time) {
+    // Some code here
+}
+// Your previous code...
+
+if (audio.uration) { // typo here
+    // Some code here
+}
+
+if (audio.duration) { // corrected
+    // Some code here
+}
+// Your previous code...
